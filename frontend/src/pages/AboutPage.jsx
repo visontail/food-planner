f713630b5203d131fd67d77f1d82fc8df76ca60c
visontail <the051425@gmail.com>
2024-04-03 11:13:18 +0200

    Update AboutPage content and styles

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -3,20 +3,38 @@ import Footer from "../components/Footer"
 
 function AboutPage() {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
+    <div className="min-h-screen flex flex-col justify-between text-default-orange">
       <Header />
       <div className="container mx-auto px-4 py-8">
         <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">How does it work?</h2>
-          <p className="text-gray-700 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam metus nunc, gravida non tincidunt eget, maximus eget odio. Suspendisse in scelerisque nibh, et facilisis justo. Phasellus aliquet urna a quam venenatis euismod. Morbi aliquet fermentum elit. Donec a scelerisque dolor. Curabitur nisl elit, lacinia ut cursus eget, bibendum sed lorem. Proin fringilla urna vel nulla condimentum bibendum. Integer at erat ultricies, pharetra neque at, accumsan mauris. Sed sodales, mi sit amet porttitor interdum, lacus lectus tincidunt metus, eu fermentum nunc sapien vitae mi.
+          <h2 className="text-xl font-semibold mb-2">Lorem Ipsum</h2>
+          <p className="text-orange-300 mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam metus nunc, gravida non tincidunt eget, maximus eget odio.
+            Suspendisse in scelerisque nibh, et facilisis justo.
+            Phasellus aliquet urna a quam venenatis euismod.
+            Morbi aliquet fermentum elit. Donec a scelerisque dolor.
+            Curabitur nisl elit, lacinia ut cursus eget, bibendum sed lorem.
+            Proin fringilla urna vel nulla condimentum bibendum.
+            Integer at erat ultricies, pharetra neque at, accumsan mauris.
           </p>
-          <h2 className="text-xl font-semibold mb-2">Why use it?</h2>
-          <p className="text-gray-700 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam metus nunc, gravida non tincidunt eget, maximus eget odio. Suspendisse in scelerisque nibh, et facilisis justo. Phasellus aliquet urna a quam venenatis euismod. Morbi aliquet fermentum elit. Donec a scelerisque dolor. Curabitur nisl elit, lacinia ut cursus eget, bibendum sed lorem. Proin fringilla urna vel nulla condimentum bibendum. Integer at erat ultricies, pharetra neque at, accumsan mauris. Sed sodales, mi sit amet porttitor interdum, lacus lectus tincidunt metus, eu fermentum nunc sapien vitae mi.
+          <h2 className="text-xl font-semibold mb-2">Lorem Ipsum</h2>
+          <p className="text-orange-300 mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam metus nunc, gravida non tincidunt eget, maximus eget odio.
+            Suspendisse in scelerisque nibh, et facilisis justo.
+            Phasellus aliquet urna a quam venenatis euismod.
+            Morbi aliquet fermentum elit. Donec a scelerisque dolor.
+            Curabitur nisl elit, lacinia ut cursus eget, bibendum sed lorem.
+            Proin fringilla urna vel nulla condimentum bibendum.
+            Integer at erat ultricies, pharetra neque at, accumsan mauris.
+            Sed sodales, mi sit amet porttitor interdum, lacus lectus tincidunt metus, eu fermentum nunc sapien vitae mi.
+            Phasellus aliquet urna a quam venenatis euismod.
+            Morbi aliquet fermentum elit. Donec a scelerisque dolor.
+            Curabitur nisl elit, lacinia ut cursus eget, bibendum sed lorem.
+            Proin fringilla urna vel nulla condimentum bibendum.
+            Integer at erat ultricies, pharetra neque at, accumsan mauris.
           </p>
-          <h2 className="text-xl font-semibold mb-2">Links</h2>
-          <a href="#" className="text-orange-400 hover:text-hover-orange">Github link</a>
         </div>
       </div>
       <Footer />
